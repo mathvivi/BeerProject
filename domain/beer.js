2452a3f1515553990022d813c26dd4350f901dec
Nriat <nicky.riat@student.uclouvain.be>
2017-04-29 18:24:24 +0200

    creation de l objet beer et ses var

--- domain/beer.js
+++ domain/beer.js
@@ -1,23 +1,43 @@
 export class Beer {
 
-  play(song) {
-    this.currentlyPlayingSong = song;
-    this.isPlaying = true;j
-  }
+var name;
+var descr;
+var cote = [];
+var degree;
 
-  pause() {
-    this.isPlaying = false;
-  }
+  setName(same) {
+    this.name = name;
+}
 
-  resume() {
-    if (this.isPlaying) {
-      throw new Error("song is already playing");
+  setDescr(descr) {
+    this.descr = descr;
+}
+
+  addCote(nmber) {
+    cote.push(nmber);
+}
+
+  meanCote() {
+    for (var i=0; i<this.cote.length; i++){
+      total += this.cote[i];
     }
-    this.isPlaying = true;
-  }
+    return total/this.cote.length;
+}
 
-  makeFavorite() {
-    this.currentlyPlayingSong.persistFavoriteStatus(true);
-  }
+  setDegree(){
+    this.degree = degree;
+}
+
+  getName(){
+    return name;
+}
+
+  getDescr(){
+  return descr;
+}
+
+  getDegree(){
+  return degree;
+}
 
 }
